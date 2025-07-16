ae844fcee99e7479d619f0e93754c97f5b6f0b90
Djeufo <chanelboris@gmail.com>
2025-07-16 14:26:57 +0200

    started

---
+++ js/dashbord.js
@@ -0,0 +1,21 @@
+function loadSection(file) {
+  fetch(file)
+    .then((response) => {
+      if (!response.ok)
+        throw new Error("HTTP " + response.status + ": " + file);
+      return response.text();
+    })
+    .then((html) => {
+      document.getElementById("content").innerHTML = html;
+    })
+    .catch((error) => {
+      document.getElementById(
+        "content"
+      ).innerHTML = `<p style="color:red;">${error.message}</p>`;
+    });
+}
+
+// Load default content on initial page load
+window.addEventListener("DOMContentLoaded", () => {
+  loadSection("/sections/home.html");
+});
